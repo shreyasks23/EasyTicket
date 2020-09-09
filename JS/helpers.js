81841c44f868edc1230654980a0b7f5fec04ac56
Shreyas K S <shreyas.ks@excelindia.com>
2020-09-09 19:31:45 +0530

    code cleaning

--- JS/helpers.js
+++ JS/helpers.js
@@ -67,3 +67,22 @@ function diff_minutes(dt2, dt1) {
     msec -= ss * 1000;
     return hh + ":" + mm + ":" + ss;
 }
+
+function Search(inputElement , tableName , SelectedIndex) {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById(inputElement);
+    filter = input.value.toUpperCase();
+    table = document.getElementById(tableName);
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[SelectedIndex];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
--- JS/ticketlist.js
+++ JS/ticketlist.js
@@ -25,21 +25,3 @@ $('#TBSearchBox').on('keyup', (event) => {
     Search(SearchBoxID, 'tblTickets', SearchIndex);
 });
 
-function Search(inputElement , tableName , SelectedIndex) {
-    let input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById(inputElement);
-    filter = input.value.toUpperCase();
-    table = document.getElementById(tableName);
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[SelectedIndex];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
-  }
