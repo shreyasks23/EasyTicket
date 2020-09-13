4e394760dbf2e1b93ee951f7100bdfb53b5de66b
shreyas ks <shreyas ks>
2020-09-13 21:14:28 +0530

    datatable changes

--- JS/ticketlist.js
+++ JS/ticketlist.js
@@ -8,70 +8,89 @@ $(function () {
             let Count = data.Count;
             console.log(Tickets);
             $("#CountHolder").text(Count);
-            $('#tblTickets').DataTable({                                
+            $('#tblTickets').DataTable({
+                ajax: '/AllTickets',
                 data: Tickets,
-                
+                rowReorder: {
+                    dataSrc: 'Subject',
+                },
+
                 columns: [
-                   
+
                     {
-                        title: "Project",
+                        //title: "Project",
                         data: "Project",
+                        edit: true
 
                     },
                     {
-                        title: "Ticket ID",
+                        // title: "Ticket ID",
                         data: "TicketID"
                     },
                     {
-                        title: "Ticket Type",
+                        //title: "Ticket Type",
                         data: "TicketType"
                     },
                     {
-                        title: "Subject",
-                        data: "Subject",                        
-                        sortable : false
+                        //title: "Subject",
+                        data: "Subject",
+                        sortable: false
                     },
 
                     {
-                        title: "Recieved Date",
+                        // title: "Recieved Date",
                         data: "RecievedDate",
                         searchable: false,
-                        
+
                     },
                     {
-                        title: "Resolved Date",
+                        //title: "Resolved Date",
                         data: "ResolvedDate",
                         searchable: false,
                     },
                     {
-                        title: "Status",
+                        //title: "Status",
                         data: "Status"
                     },
                     {
-                        title: "Priority",
+                        //title: "Priority",
                         data: "Priority"
                     },
                     {
-                        title: "Severity",
+                        //title: "Severity",
                         data: "Severity"
                     },
                     {
-                        title: "Resolution Time",
+                        // title: "Resolution Time",
                         data: "ResolutionTime",
                         searchable: false,
                     },
                     {
-                        title: "Handled By",
+                        //title: "Handled By",
                         data: "HandledBy"
+                    },
+                    {
+                        //title: "Handled By",
+                        render: function (data, type, full, meta) {
+
+                            return '<button id="editBtn" class="btn btn-wrang btn-flat edit" name="editBtn" type="button">Edit</button>' +
+                                '&nbsp;&nbsp;<button id="deleteBtn" class="btn btn-danger btn-flat delete" name="deleteBtn" type="button" >Delete</button>';
+
+                        }
                     }
                 ]
-            });           
+            });
         },
         error: (err) => {
             console.log(err);
         }
-    }); 
-    
+    });
+    $('#tblTickets').on('click', '.delete', function () {
+        var table = $('#tblTickets').DataTable();
+        table.row($(this).parents('tr')).remove().draw(false); //command for delete all that row
+    });    
+  
+
 
     $('#BtnExport').on('click', (event) => {
         ExportToExcel("tblTickets");
